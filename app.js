87ff8bf6093e4b6cab8be48882f1e828976c21d1
Gabriel Mathias da SILVA BASTOS <102390289+Gab2390@users.noreply.github.com>
2024-09-05 12:15:59 -0300

    Add files via upload
     O meu projeto da imersão com a escola ALURA

---
+++ app.js
@@ -0,0 +1,10 @@
+let dados = [
+    {
+        titulo: "Nirvana",
+        descricao: "O Nirvana foi uma banda de rock norte-americana que revolucionou a música nos anos 90. Com Kurt Cobain no vocal e guitarra, Krist Novoselic no baixo e Dave Grohl na bateria, o trio de Seattle liderou o movimento grunge, um som caracterizado por guitarras distorcidas, letras introspectivas e uma atitude rebelde. Álbuns como 'Nevermind' e hits como 'Smells Like Teen Spirit' os catapultararam para a fama mundial, mas a morte trágica de Cobain em 1994 marcou o fim prematuro da banda, que deixou um legado duradouro na história do rock."
+    },
+    {
+        titulo: "Alice in Chains",
+        descricao: "Os Alice in Chains foram uma das bandas mais influentes do movimento grunge, ao lado do Nirvana. Com Layne Staley no vocal, Jerry Cantrell na guitarra, Mike Starr no baixo e Sean Kinney na bateria, o quarteto de Seattle conquistou fãs ao redor do mundo com seu som pesado e letras introspectivas, muitas vezes abordando temas como vício e angústia. Hits como 'Man in the Box' e 'Would?' os tornaram um dos pilares do grunge, e seu legado continua a inspirar novas gerações de músicos."
+    }
+];
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    "nirvana",
+    "alice inchains"
+];
+
+let nirvana = {
+    titulo: "uma banda grunge",
+    ano: 1987,
+    integrantes: ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"]
+};
+
+let aliceInChains = {
+    titulo: "uma banda de rock alternativo",
+    ano: 1987,
+    integrantes: ["Layne Staley", "Jerry Cantrell", "Mike Inez"]
+};
+
+// Adicionando a banda clássica Alice In Chains
+let aliceInChainsClassica = {
+    titulo: "uma banda clássica de rock",
+    ano: 1990,
+    integrantes: ["Layne Staley", "Jerry Cantrell", "Mike Inez", "Sean Kinney"]
+};
